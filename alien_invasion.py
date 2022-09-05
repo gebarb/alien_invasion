47762ac9ad6912f0f3f48565ddfd81f15edc009c
Grayson <ghebarb@gmail.com>
2022-09-05 13:39:57 -0500

    exit with esc

--- alien_invasion.py
+++ alien_invasion.py
@@ -70,7 +70,7 @@ class AlienInvasion:
             self.ship.moving_right = True
         elif event.key == pygame.K_LEFT:
             self.ship.moving_left = True
-        elif event.key == pygame.K_q:
+        elif event.key == pygame.K_q or event.key == pygame.K_ESCAPE:
             sys.exit()
         elif event.key == pygame.K_SPACE:
             self.firing = True
